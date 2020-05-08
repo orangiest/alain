330927f23d5dcc748ddf1645f83ad3cd37750065
BuildTools <unconfigured@null.spigotmc.org>
2020-05-08 18:58:30 +0800

    modify file

--- src/app/routes/routes-routing.module.ts
+++ src/app/routes/routes-routing.module.ts
@@ -27,7 +27,6 @@ const routes: Routes = [
       { path: 'exception', loadChildren: () => import('./exception/exception.module').then(m => m.ExceptionModule) },
       // 业务子模块
       // { path: 'widgets', loadChildren: () => import('./widgets/widgets.module').then(m => m.WidgetsModule) },
-      { path: 'user', loadChildren: () => import('./commons/commons.module').then(m => m.CommonsModule) },
       { path: 'resources', loadChildren: () => import('./resources/resources.module').then(m => m.ResourcesModule) },
 
     ]
